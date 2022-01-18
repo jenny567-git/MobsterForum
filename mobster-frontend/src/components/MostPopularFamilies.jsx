8464bd1fce852fd2c3adf93d44ec9cc8b439d2b9
Yahya Allouhaibi <yahya.allouhaibi@edu.newton.se>
2022-01-18 13:28:58 +0000

    Merged PR 2: 19- created frontend structure and downloaded routes
    Related work items: #45

---
+++ mobster-frontend/src/components/MostPopularFamilies.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function MyFamilies() {
+    return (
+        <div className="mp-families">
+            <h3>Most Popular Families</h3>
+            <ul>
+                <li>family 1</li>
+                <li>family 2</li>
+                <li>family 3</li>
+                <li>family 4</li>
+                <li>family 5</li>
+                <li>family 6</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MyFamilies
