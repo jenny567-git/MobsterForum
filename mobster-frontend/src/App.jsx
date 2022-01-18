78c18a9cba587671ba55fd507e02b752b3b6d30c
yahya allouhaibi <73111505+yahyaallouhaibi@users.noreply.github.com>
2022-01-18 09:59:30 +0100

    installed routes, and added the home route

--- mobster-frontend/src/App.jsx
+++ mobster-frontend/src/App.jsx
@@ -1,10 +1,21 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Header from './components/Header'
+import Home from './pages/Home'
 function App() {
 
   return (
-    <div className="App">
-      <Header></Header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header></Header>
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+              
+            
+          </Routes>
+        </div>
+      </div>
+    </Router>
   )
 }
 
