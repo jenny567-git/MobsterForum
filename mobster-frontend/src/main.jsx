f9a490e0b34a641e02d63d3af7689a024b988e10
Elin WinrothOrtega <elin.winrothortega@edu.newton.se>
2022-01-24 12:15:42 +0000

    Merged PR 6: Login Auth0 frontend
    added the code for auth0 login

--- mobster-frontend/src/main.jsx
+++ mobster-frontend/src/main.jsx
@@ -2,10 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { Auth0Provider } from "@auth0/auth0-react";
+
+import { getConfig } from './config';
+
+const config = getConfig();
+
+const providerConfig = {
+  domain: config.domain,
+  clientId: config.clientId,
+  ...(config.audience ? { audience: config.audience } : null),
+  redirectUri: window.location.origin,
+ 
+};
 
 ReactDOM.render(
   <React.StrictMode>
+    <Auth0Provider {...providerConfig}
+   
+  >
     <App />
+  </Auth0Provider>,
   </React.StrictMode>,
   document.getElementById('root')
 )
