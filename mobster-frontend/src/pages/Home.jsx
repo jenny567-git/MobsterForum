f44da0c674507b35b9279a51518786d4ce031179
yahya allouhaibi <73111505+yahyaallouhaibi@users.noreply.github.com>
2022-01-25 13:29:44 +0100

    fetch myFamilies by user should work after fixing the database, commented for now

--- mobster-frontend/src/pages/Home.jsx
+++ mobster-frontend/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import MyFamilies from '../components/MyFamilies'
+//import MyFamilies from '../components/MyFamilies'
 import MostPopularFamilies from '../components/MostPopularFamilies'
 import AddThread from '../components/AddThread/AddThread'
 import Thread from '../components/Thread/Thread'
+//import { useAuth0 } from '@auth0/auth0-react';
 function Home() {
+    /*const {
+        isAuthenticated
+    } = useAuth0();*/
     return (
         <div className="home">
-            <MyFamilies />
+            {/* {isAuthenticated && (<MyFamilies />)} */}
             <div className="page-center">
                 <AddThread />
                 <Thread username="user1" familyName="family 1" threadDate="22/05/2022" title="hej" text="hejdå" ></Thread>
