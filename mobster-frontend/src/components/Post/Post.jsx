24cfa3dbb9a385212a67f95c913a4b1a1ebc5b08
giddster <73110741+giddster@users.noreply.github.com>
2022-01-27 16:15:16 +0100

    added request to get comments for a specific post. added and changed some styling. also fixed bad controller endpoint for postscontroller

--- mobster-frontend/src/components/Post/Post.jsx
+++ mobster-frontend/src/components/Post/Post.jsx
@@ -2,24 +2,34 @@ import React from 'react';
 import  "./Post-styling.css"
 import logo from '../../assets/Mobster-logo.png'
 import { Button } from 'react-bootstrap';
+import useFetch from '../../CustomHooks/useFetch';
 
-export const Post = () => {
+export const Post = ({ id }) => {
+  const { data: posts, error, isPending } = useFetch(`https://localhost:44304/api/Posts/thread/${id}`);
   
-    return <div className='single-post'>
+    return <div>
             
-            <div className="avatar-container">
-                <img className='avatar' src={logo} alt="profile picture" />
-            </div>
-            
-            <div className="post-content">
-                <p className='post-metadata'>Posted by <strong className='post-metadata-bold'>USERNAME</strong> at CREATEDAT</p>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem voluptatum sed sequi consectetur distinctio? Aliquid, odio. Natus nesciunt, repudiandae reprehenderit, magni officiis vel culpa corporis eius architecto vitae, laborum commodi.</p>
-            </div>
-            
-            <div className="post-buttons">
-                <Button className='post-btn' title='Flag post as inappropriate'><i class="fas fa-flag"></i></Button>
-                <Button className='post-btn' title='Censor post content'><i class="fas fa-comment-slash"></i></Button>
-            </div>
+            { isPending && <div>Loading posts...</div> }
+
+            { error && <div>There are no posts on this thread yet...</div> }
+
+            { posts && posts.map((post) => 
+                <div className='single-post'>
+                    <div className="avatar-container">
+                        <img className='avatar' src={logo} alt="profile picture" />
+                    </div>
+                    
+                    <div className="post-content">
+                        <p className='post-metadata'>Posted by <strong className='post-metadata-bold'>{ post.author.userName }</strong> at { post.createdAt }</p>
+                        <p>{ post.content }</p>
+                    </div>
+                    
+                    <div className="post-buttons">
+                        <Button className='post-btn' title='Flag post as inappropriate'><i class="fas fa-flag"></i></Button>
+                        <Button className='post-btn' title='Censor post content'><i class="fas fa-comment-slash"></i></Button>
+                    </div>
+                </div>
+            )}
         
         </div>;
 };
--- mobster-frontend/src/pages/SingleThreadView/SingleThreadView.jsx
+++ mobster-frontend/src/pages/SingleThreadView/SingleThreadView.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import { Post } from '../../components/Post/Post';
 import useFetch from '../../CustomHooks/useFetch';
@@ -7,34 +8,48 @@ import "./SingleThreadView-styling.css"
 const SingleThreadView = () => {
   const { id } = useParams();
   const { data: thread, error, isPending } = useFetch(`https://localhost:44304/api/Thread/${id}`);
+  const [isVisible, setVisible] = useState(false);
+
+  const toggleReplyBox = () => {
+    setVisible(!isVisible);
+  }
   
   return <div className="SingleThreadView">
-    <div className="thread-container">
-        { isPending && <div>Loading thread...</div> }
-        { error && <h2>Error: no content found.</h2> }
-        
-        { thread && (<div className="thread">
-          <p className="thread-metadata"> 
-            Posted by <strong className='thread-metadata-bold'>{thread.author.userName}</strong> in <em className='thread-metadata-bold'>{thread.family.name}</em>  at {thread.createdAt}
-          </p>
-          
-          <h2>{ thread.title}</h2>
-          <p>{ thread.content }</p>
-          
-          <div className="thread-btns">
-            <button>Reply</button>
-            <button>Report</button>
-          </div>
+    
+    <div className="main-container">
+        <div className="thread-container">
+            { isPending && <div>Loading thread...</div> }
+            { error && <h2>Error: no content found.</h2> }
+            
+            { thread && (<div className="thread">
+              <p className="thread-metadata"> 
+                Posted by <strong className='thread-metadata-bold'>{thread.author.userName}</strong> in <em className='thread-metadata-bold'>{thread.family.name}</em>  at {thread.createdAt}
+              </p>
+              
+              <h2>{ thread.title}</h2>
+              <p>{ thread.content }</p>
+              
+            </div>
+          )}
         </div>
-      )}
-      <div className="thread-posts">
-        <Post />
-      </div>
-      {/* <div className="thread-reply">
-        <textarea id="reply-textarea" cols="60" rows="5"></textarea>
-        <button>post reply</button>
-      </div> */}
-  </div>
+      
+        <div className="thread-btns">
+                <Button className='thread-btns' title='Post reply' onClick={toggleReplyBox}><i class="fas fa-reply"></i></Button>
+                <Button className='thread-btns' title='Flag thread as inappropriate'><i class="fas fa-flag"></i></Button>
+                <Button className='thread-btns' title='Censor thread content'><i class="fas fa-comment-slash"></i></Button>
+        </div>
+
+        {/* TODO: break out the create post reply to separate component */}
+        <div className={isVisible ? 'thread-reply' : 'invisible'} >
+              <textarea id="reply-textarea" cols="60" rows="5"></textarea>
+              <button>Post reply</button>
+        </div>
+        
+        <div className="thread-posts">
+            <Post id={id} />
+        </div>
+    </div>
+    
 </div>;
 }
 
