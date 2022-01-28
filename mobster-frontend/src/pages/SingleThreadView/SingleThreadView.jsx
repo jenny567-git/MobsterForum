5ae26a1dd95298f2571a0b4dfed1123a633c57fc
giddster <73110741+giddster@users.noreply.github.com>
2022-01-28 13:19:46 +0100

    manual merge conflict handling before merge from feature/posts

--- mobster-frontend/src/pages/SingleThreadView/SingleThreadView.jsx
+++ mobster-frontend/src/pages/SingleThreadView/SingleThreadView.jsx
@@ -1,8 +1,9 @@
 import React, {useState, useEffect } from 'react'
-import { useParams,useNavigate } from 'react-router-dom';
-import "./SingleThreadView-styling.css"
+import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios'
 import { useAuth0 } from '@auth0/auth0-react'; 
+import { Button } from 'react-bootstrap';
+import "./SingleThreadView-styling.css"
 
 const SingleThreadView = () => {
   const { id } = useParams();
@@ -14,6 +15,7 @@ const SingleThreadView = () => {
   const {user, isLoading} = useAuth0();
   let navigate = useNavigate();
 
+  // TODO: add error so that "no content found" can be rendered on error
   const fetchThreadById = (async ()=> {
     let response = await axios.get(`https://localhost:44304/api/Thread/${id}`) 
     response.data.createdAt = response.data.createdAt.slice(0,10)
@@ -53,49 +55,76 @@ const SingleThreadView = () => {
     
   })
 
+  const toggleReplyBox = () => {
+    // setVisible(!isVisible);
+    scrollToBottom();
+  }
+
+  function getThreadLink(){
+    navigator.clipboard.writeText(window.location.href);
+  }
+
+  function scrollToBottom(){
+    let scrollingElement = (document.scrollingElement || document.body);
+    scrollingElement.scrollTop = scrollingElement.scrollHeight;
+  }
+
+  function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+
   useEffect(()=>{
     fetchThreadById();
   },[])
   if(isLoading){
-    return <div> <p>loading</p></div>
+    return <div> <p>Loading thread...</p></div>
   }
   return <div className="SingleThreadView">
-    <div className="thread-container">
-        
-        { thread && (<div className="thread">
-          <p className="thread-metadata"> 
-            Posted by <strong className='thread-metadata-bold'>{thread.author.userName}</strong> in <em className='thread-metadata-bold'>{thread.family.name}</em>  at {thread.createdAt}
-          </p>
-          { isReadOnly && (<div className='thread-data'>
-            <input value={threadTitle} readOnly></input>
-            <textarea value={threadContent} readOnly></textarea>
-          </div>)}
-          
-          {!isReadOnly && (<div className='thread-data'>
-            <input value={threadTitle} onChange={e => setThreadTitle(e.target.value)}></input>
-            <textarea value={threadContent} onChange={e => setThreadContent(e.target.value)}></textarea>
-          </div>)}
-          
-          <div className="thread-btns">
-            {thread.author.authId == user.sub && (
-              <div className='crud-btns'>
-                <button onClick={editThread}>{btnText}</button>
-                <button onClick={deleteThread}><i className="fas fa-trash-alt"></i></button>
+    
+        <div className="main-container">
+            <div className="thread-container">
+            
+            { thread && (<div className="thread">
+              <p className="thread-metadata"> 
+                Posted by <strong className='thread-metadata-bold'>{thread.author.userName}</strong> in <em className='thread-metadata-bold'>{thread.family.name}</em>  at {thread.createdAt}
+              </p>
+              { isReadOnly && (<div className='thread-data'>
+                <input value={threadTitle} readOnly></input>
+                <textarea value={threadContent} readOnly></textarea>
+              </div>)}
+              
+              {!isReadOnly && (<div className='thread-data'>
+                <input value={threadTitle} onChange={e => setThreadTitle(e.target.value)}></input>
+                <textarea value={threadContent} onChange={e => setThreadContent(e.target.value)}></textarea>
+              </div>)}
+              
+              <div className="thread-btns">
+                {thread.author.authId == user.sub && (
+                  <div className='crud-btns'>
+                    <button onClick={editThread}>{btnText}</button>
+                    <button onClick={deleteThread}><i className="fas fa-trash-alt"></i></button>
+                  </div>
+                )}
+                <Button className='thread-btns' title='Post reply' onClick={toggleReplyBox}><i class="fas fa-reply"></i></Button>
+                <Button className='thread-btns' title='Share link' onClick={getThreadLink}><i class="fas fa-share-square"></i></Button>
+                <Button className='thread-btns' title='Report thread'><i className="fas fa-exclamation"></i></Button>
+                <Button className='thread-btns' title='Censor thread content'><i class="fas fa-comment-slash"></i></Button>
               </div>
-            )}
-            <button><i className="fas fa-reply"></i></button>
-            <button><i className="fas fa-exclamation"></i></button>
+            </div>
+          )}
+          <div className="thread-posts">
+                <Post id={id} />
+            </div>
+
+            <div className='thread-reply' >
+                  <CreatePost />
+            </div>
+
+            <Button className='backtotop-button' onClick={scrollToTop}>Back to top</Button>
           </div>
         </div>
-      )}
-      <div className="thread-posts">
-        
-      </div>
-      {/* <div className="thread-reply">
-        <textarea id="reply-textarea" cols="60" rows="5"></textarea>
-        <button>post reply</button>
-      </div> */}
-  </div>
+    
 </div>;
 }
 
