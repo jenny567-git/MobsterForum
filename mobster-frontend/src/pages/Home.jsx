fc74a769f80219284a0cab0ad40f937731c81232
Jenny Wong <jennywong567@gmail.com>
2022-02-02 08:59:42 +0100

    fixed error rendering myFamilies

--- mobster-frontend/src/pages/Home.jsx
+++ mobster-frontend/src/pages/Home.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-//import MyFamilies from '../components/MyFamilies'
+import MyFamilies from '../components/MyFamilies'
 import MostPopularFamilies from '../components/MostPopularFamilies'
 import AddThread from '../components/AddThread/AddThread'
 import Thread from '../components/Thread/Thread'
-//import { useAuth0 } from '@auth0/auth0-react';
+import { useAuth0 } from '@auth0/auth0-react';
 function Home() {
-    /*const {
+    const {
         isAuthenticated
-    } = useAuth0();*/
+    } = useAuth0();
+    
     return (
         <div className="home">
-            {/* {isAuthenticated && (<MyFamilies />)} */}
+            {isAuthenticated && (<MyFamilies />)}
             <div className="page-center">
                 <AddThread />
                 <Thread username="user1" familyName="family 1" threadDate="22/05/2022" title="hej" text="hejdå" ></Thread>
