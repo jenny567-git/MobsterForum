facbca296dfdf38ca8c13fa5ca345960a2287a08
Daniel Riddersporre <56501881+Poodlepaag@users.noreply.github.com>
2022-02-02 09:45:40 +0100

    Design and all elements are in place

--- mobster-frontend/src/pages/Admin-dashboard.jsx
+++ mobster-frontend/src/pages/Admin-dashboard.jsx
@@ -4,7 +4,8 @@ import React, { useEffect, useState } from "react";
 function AdminDashboard() {
 
     const [users, setUsers] = useState([]);
-    let [isLoaded, setState] = useState(false)
+    const [filteredUsers, setFilteredUsers] = useState([]);
+    let [isLoaded, setLoadState] = useState(false);
 
     const fetchUsers = async () => {
         const response = await axios.get('https://localhost:44304/api/User/allUsers');
@@ -16,28 +17,43 @@ function AdminDashboard() {
         console.log(user);
         const response = await axios.put(`https://localhost:44304/api/Block?${user.userId}`);
         console.log(response);
+        setLoadState(false);
     }
 
     useEffect(() => {
         if(isLoaded == false){
             fetchUsers();
         }
-        setState(true);
+        setLoadState(true);
     })
 
     return (
-        <div>
-            <h3>Users</h3>
-            {users.map( (user,index) => (
-                <div key={index} className="wrapper">
-                    <p>
-                    {user.userName}, 
-                    {user.isBanned.toString()}
-                    </p>
-                    <button value={user} onClick={() => toggleUserBlock(user)}>TEST</button>
+        <div className="outer-wrapper">
+            <div className="inner-wrapper">
+                <div className="users-wrapper">
+                    <div className="users-header">
+                        <h3>Application User Block</h3>
+                        <p>Block a Mobster user and give them the "Mark of a traitor" which blocks them from communicating with other members in every part of the application</p>
+                        <hr></hr>
+                    </div>
+                    <div className="users-search">
+                        <input type="text" placeholder="search user" className="searchbar"/>
+                    </div>
+                    <div className="users-list">
+                        {users.map( (user,index) => (
+                            <div key={index} className="user-detail">
+                                {user.userName}, 
+                                {user.isBanned.toString()}
+                                {user.isBanned ? (
+                                    <button value={user} onClick={() => toggleUserBlock(user)} className="button unblock">Unblock</button>
+                                ) : (
+                                    <button value={user} onClick={() => toggleUserBlock(user)} className="button block">Block</button>
+                                )}
+                            </div>
+                        ))}
+                    </div>
                 </div>
-            )
-            )}
+            </div>
         </div>
     )
 }
