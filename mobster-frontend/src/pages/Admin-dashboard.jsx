f5acd1fd952376172d4de3ada98cdc343e3ff982
Daniel Riddersporre <56501881+Poodlepaag@users.noreply.github.com>
2022-02-02 07:08:22 +0100

    created dashboard

--- mobster-frontend/src/pages/Admin-dashboard.jsx
+++ mobster-frontend/src/pages/Admin-dashboard.jsx
@@ -1,16 +1,43 @@
-import React from "react";
-
-const selectedUser = "4789243e-72fd-448e-b51b-ccafe4973717";
-
-function ToggleBlockOnUser() {
-    
-}
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function AdminDashboard() {
+
+    const [users, setUsers] = useState([]);
+    let [isLoaded, setState] = useState(false)
+
+    const fetchUsers = async () => {
+        const response = await axios.get('https://localhost:44304/api/User/allUsers');
+        console.log(response);
+        setUsers(response.data);
+    }
+
+    async function toggleUserBlock(user) {
+        console.log(user);
+        const response = await axios.put(`https://localhost:44304/api/Block?${user.userId}`);
+        console.log(response);
+    }
+
+    useEffect(() => {
+        if(isLoaded == false){
+            fetchUsers();
+        }
+        setState(true);
+    })
+
     return (
-        <div class="main-wrapper">
-            <h3>Admin dashboard</h3>
-            <button onClick={ToggleBlockOnUser()}>Block user</button>
+        <div>
+            <h3>Users</h3>
+            {users.map( (user,index) => (
+                <div key={index} className="wrapper">
+                    <p>
+                    {user.userName}, 
+                    {user.isBanned.toString()}
+                    </p>
+                    <button value={user} onClick={() => toggleUserBlock(user)}>TEST</button>
+                </div>
+            )
+            )}
         </div>
     )
 }
