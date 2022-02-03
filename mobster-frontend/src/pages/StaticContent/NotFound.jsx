d43ecbd8b90561f52aa9f29241ec45d31ccce88f
Christian Elm <christian.elm@edu.newton.se>
2022-02-03 12:11:13 +0000

    Merged PR 19: Merge static pages into main
    Simple static pages with content
    
    Related work items: #110, #115

---
+++ mobster-frontend/src/pages/StaticContent/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Image } from 'react-bootstrap';
+import './StaticContent-styles.css';
+
+
+export const NotFound = () => {
+  return <div>
+      <div className="notfound-container">
+          <h1>You lost, or what? That page don't exist! Get outta here, ya mook!</h1>
+          <Image src="https://i.pinimg.com/236x/ab/b6/0d/abb60d5cb3df67c8e0d48928ac9e0302--deniro-good-fellas.jpg" />
+      </div>
+      
+  </div>;
+};
