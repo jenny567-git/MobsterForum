bc221d7dce84663db76e59738ca34abae0d6cf2d
Elin Winroth Ortega <73227921+ElinOrtega@users.noreply.github.com>
2022-02-04 13:53:22 +0100

    searchbar, select type css

--- mobster-frontend/src/components/Search/SearchBar.jsx
+++ mobster-frontend/src/components/Search/SearchBar.jsx
@@ -3,7 +3,6 @@ import axios from "axios";
 import { Context } from "../../utils/store";
 import { useNavigate } from "react-router-dom";
 
-
 function SearchBar() {
   const [searchString, setSearchString] = useState("");
   const [context, updateContext] = useContext(Context);
@@ -15,27 +14,33 @@ function SearchBar() {
     const radiovalue = document.querySelector(
       'input[type="radio"]:checked'
     ).value;
-    
+
     switch (radiovalue) {
       case "threads":
-        axios.get(`https://localhost:44304/api/Thread?searchstring=${searchString}`)
-        .then((res) => {
-          console.log("Success: ", res.data);
-          updateContext({searchResult: res.data, searchType:radiovalue})
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-        });
+        axios
+          .get(
+            `https://localhost:44304/api/Thread?searchstring=${searchString}`
+          )
+          .then((res) => {
+            console.log("Success: ", res.data);
+            updateContext({ searchResult: res.data, searchType: radiovalue });
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
         break;
       default:
-        axios.get(`https://localhost:44304/api/Family?searchstring=${searchString}`)
-        .then((res) => {
-          console.log("Success: ", res.data);
-          updateContext({searchResult: res.data, searchType:radiovalue})
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-        });
+        axios
+          .get(
+            `https://localhost:44304/api/Family?searchstring=${searchString}`
+          )
+          .then((res) => {
+            console.log("Success: ", res.data);
+            updateContext({ searchResult: res.data, searchType: radiovalue });
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
         break;
     }
     navigate("/searchresult");
@@ -44,11 +49,11 @@ function SearchBar() {
   const onSubmit = async (e) => {
     e.preventDefault();
     search(e);
-  }
+  };
 
   return (
     <div className="search-bar-input">
-      <form onSubmit={onSubmit}>
+      <form className="select-type" onSubmit={onSubmit}>
         <input
           id="searchText"
           type="text"
@@ -61,22 +66,23 @@ function SearchBar() {
           <i className="fas fa-search"></i>
         </button>
 
-        <p>Select type:</p>
-        <div>
-          <input
-            type="radio"
-            id="huey"
-            name="drone"
-            value="families"
-            defaultChecked
-          />
-          <label htmlFor="huey">Family</label>
-        </div>
-
-        <div>
-          <input type="radio" id="dewey" name="drone" value="threads" />
-          <label htmlFor="dewey">Thread</label>
-        </div>
+       
+          <p>Select:</p>
+          <div className="radio-button">
+            <input
+              type="radio"
+              id="huey"
+              name="drone"
+              value="families"
+              defaultChecked
+            />
+            <label  htmlFor="huey">Family</label>
+          </div>
+          <div className="radio-button">
+            <input type="radio" id="dewey" name="drone" value="threads" />
+            <label htmlFor="dewey">Thread</label>
+          </div>
+        
       </form>
     </div>
   );
